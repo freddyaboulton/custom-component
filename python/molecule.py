c773ae6c8c7e2e6d3a87d30c7ed9f0ac29dcdc48
freddyaboulton <alfonsoboulton@gmail.com>
2023-05-21 22:12:45 -0400

    Add data type

--- python/molecule.py
+++ python/molecule.py
@@ -1,6 +1,17 @@
 from gradio import Component
 from gradio import JSONSerializable
 import moleculelib
+from pydantic import BaseModel
+
+
+# The types accepted by our components are not 
+# documented well.
+# We should expose our existing datatypes better,
+# e.g. ImageData, GalleryData, CodeData so that
+# component authors can use them in their components.
+# We should also let them define their own data models.
+class MoleculeData(BaseModel):
+    pass
 
 
 # Component defines change events so all components
@@ -10,13 +21,13 @@ class MoleculeViewer(Component, JSONSerializable):
     # This automatically adds .edit and .scroll methods?
     EVENTS = ["edit", "scroll"]
 
-    def preprocess(self, data: dict):
+    # We should expose 
+    def preprocess(self, data: MoleculeData):
         return moleculelib.generate(**data)
 
     # This is the default impl in Component
     def postprocess(self, data: dict):
         return data
     
-    # This
     def update(self, *args, **kwargs):
         pass
